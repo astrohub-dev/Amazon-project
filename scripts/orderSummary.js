248c78e1a7467d89748df9fe91e36129fba172a5
NduJiovita <ndujiovita1@gmail.com>
2025-05-15 17:12:56 +0100

    api call

--- scripts/orderSummary.js
+++ scripts/orderSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { deliveryOptions, getDeliveryOption } from '../data/deliveryOptions.js';
 import {renderOrderSummary} from './checkout.js';
+import { addOrder } from '../data/orders.js';
 
 export function displayCash() {
 
@@ -63,12 +64,35 @@ export function displayCash() {
             <div class="payment-summary-money">$${(orderTotal).toFixed(2)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
             Place your order
         </button>
     `;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+    document.querySelector('.js-place-order').addEventListener('click', async () => {
+       try{
+            const response = await fetch('https://supersimplebackend.dev/orders', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify({
+                    cart: cart
+                })
+            })
+
+            const order = await response.json();
+            addOrder(order);
+       
+       } catch (error) {
+        console.log('unexpected error. Please try again later.');
+      }
+       
+      window.location.href = 'orders.html'
+
+    });
 }
 
 displayCash();
